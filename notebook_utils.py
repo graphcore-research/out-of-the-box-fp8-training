2d7e8f2824ff6fdb5c8ddf617e083250913d6f68
charlieb <charlieb@graphcore.ai>
2023-07-13 22:52:46 +0100

    temporary commit - wip

--- notebook_utils.py
+++ notebook_utils.py
@@ -71,7 +71,7 @@ def plot(df: pd.DataFrame) -> matplotlib.axes.Axes:
     return p
 
 
-def train(model: nn.Module, **config_overrides: Any) -> None:
+def train(model: nn.Module, **config_overrides: Any) -> pd.DataFrame:
     experiment_name = _gen_experiment_name(model) + config_overrides.pop(
         "experiment_name_suffix", ""
     )
@@ -108,3 +108,66 @@ def train(model: nn.Module, **config_overrides: Any) -> None:
     valid_df["Train/Valid"] = "Valid"
     df = pd.concat([train_df, valid_df])
     plot(df)
+
+
+# TODO: rewrite all of this properly
+import pickle
+
+from torch import LongTensor, Tensor
+
+import example_text
+
+
+def example_seqs(batch_size):
+    text = example_text.hansard_lords_1964
+    seq_len = len(text) // batch_size
+    return [text[i * seq_len : (i + 1) * seq_len] for i in range(batch_size)]
+
+
+def create_model_inputs(
+    tokenize_fn,
+    example_seqs,
+    max_seq_len: int,
+):
+    # Although we can't crop the example sequences exactly based on `max_seq_len`
+    # (we don't know how many tokens will be produced), we assume that the tokenizer
+    # will produce > (len(pre_tok_seq) / 10) tokens and crop accordingly.
+    # his just avoids the tokenizer having to do too much unnecessary work
+    pre_tok_seqs = [pre_tok_seq[: 10 * max_seq_len] for pre_tok_seq in example_seqs]
+    # try:
+    #     seqs = tokenize_fn(pre_tok_seqs)
+    # except TypeError:
+    seqs = [tokenize_fn(s) for s in pre_tok_seqs]
+
+    seq_len = min(max_seq_len, max(len(s) for s in seqs)) + 1
+
+    # The tokenize function may produce a ready tensor. However in most cases it gives a
+    # list. This means we have to turn this into a tensor manually
+    if not isinstance(seqs, Tensor):
+        truncated_seqs = []
+        for s in seqs:
+            new_s = s[:seq_len]
+            if len(new_s) < seq_len:  # handles padding (unlikely wth example txt)
+                new_s += tokenize_fn(" ")[0] * (seq_len - len(new_s))
+            truncated_seqs.append(new_s)
+        seqs = LongTensor(truncated_seqs)
+    input_idxs = seqs[:, : seq_len - 1].clone()
+    labels = seqs[:, 1:seq_len].clone()
+    return input_idxs, labels
+
+
+def demo_data(batch_size=64):
+    with open("nanoGPT/data/shakespeare_char/meta.pkl", "rb") as f:
+        meta = pickle.load(f)
+        stoi = meta["stoi"]
+
+    def encode(s):
+        return [stoi.get(c, stoi[" "]) for c in s]
+
+    seqs = example_seqs(batch_size)
+    inputs, labels = create_model_inputs(
+        encode,
+        seqs,
+        max_seq_len=_general_config_dict["block_size"],
+    )
+    return inputs, labels
